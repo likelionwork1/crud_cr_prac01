7471f19a6fe08cf1dc2cf0f1eaa31f09c3b33d62
likelionwork1 <ubuntu@ip-172-31-22-75.us-east-2.compute.internal>
2019-07-01 18:01:34 +0000

    prac01_form

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,12 +4,12 @@ class HomesController < ApplicationController
 
   def second
     one_post = Post.new
-    one_post.title = params[:title]
-    one_post.content = params[:content]
-    one_post.numb = params[:numb]
-    one_post.save
     
-    redirect_to "/homes/index"
+    # 아래를 빈칸을 완성하여 데이터가 저장되도록 하고, 데이터 저장 후 초기화면(index페이지)으로 자동으로 돌아가기
+    
+    
+    
+    # 여기까지
   end
   
   def read
